e6e83b91b12c717b1f06d0629ab14ba63f4abd88
hornokattila <hornokattila@outlook.com>
2021-12-11 00:35:53 +0100

    Record implementation.

--- src/main/java/application/controller/GungnirController.java
+++ src/main/java/application/controller/GungnirController.java
@@ -3,7 +3,6 @@ package application.controller;
 import application.entity.gungnir.Gungnir;
 import application.entity.skofnung.database.Address;
 import application.repository.GungnirRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,11 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 
 @RestController
-@RequiredArgsConstructor
-public class GungnirController {
-
-    private final GungnirRepository gungnirRepository;
-
+public final record GungnirController(GungnirRepository gungnirRepository) {
     @PostMapping("/gungnirs/search/findByAddress")
     public Gungnir findByAddress(@RequestBody @Valid Address address) {
         return gungnirRepository.findByAddress(address);
