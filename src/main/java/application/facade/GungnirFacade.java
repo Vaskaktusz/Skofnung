875f82281dce71e92fd4fbb89d7dab02f4f61da4
hornokattila <hornokattila@outlook.com>
2021-12-12 15:15:22 +0100

    GungnirFacade service.

---
+++ src/main/java/application/facade/GungnirFacade.java
@@ -0,0 +1,40 @@
+package application.facade;
+
+import application.entity.skofnung.database.Address;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.AbstractGenericHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Collections;
+
+@Service
+public final class GungnirFacade {
+    public <K> K bodyToMono(Address address, Class<K> clazz, String contextPath) {
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getMessageConverters().add(httpMessageConverterToMono());
+        ResponseEntity<K> responseEntity = restTemplate.exchange(
+                address.getLocation().concat(contextPath),
+                HttpMethod.GET,
+                headersToMono(address.getUsername(), address.getPassword()),
+                clazz);
+        return responseEntity.getBody();
+    }
+
+    private HttpEntity<String> headersToMono(String username, String password) {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setBasicAuth(username, password);
+        return new HttpEntity<>("body", httpHeaders);
+    }
+
+    private AbstractGenericHttpMessageConverter<Object> httpMessageConverterToMono() {
+        AbstractGenericHttpMessageConverter<Object> httpMessageConverter = new MappingJackson2HttpMessageConverter();
+        httpMessageConverter.setSupportedMediaTypes(Collections.singletonList(MediaType.TEXT_HTML));
+        return httpMessageConverter;
+    }
+}
--- src/main/java/application/repository/GungnirRepository.java
+++ src/main/java/application/repository/GungnirRepository.java
@@ -5,52 +5,24 @@ import application.entity.gungnir.metadata.Details;
 import application.entity.gungnir.metadata.Device;
 import application.entity.gungnir.metadata.System;
 import application.entity.skofnung.database.Address;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.AbstractGenericHttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import application.facade.GungnirFacade;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.Collections;
 
 @Repository
+@RequiredArgsConstructor
 public class GungnirRepository {
+    private final GungnirFacade gungnirFacade;
+
     public Gungnir findByAddress(Address address) {
         Gungnir gungnir = new Gungnir();
-        gungnir.setDetails(bodyToMono(address, Details.class, "/api/detail"));
-        gungnir.setDevice(bodyToMono(address, Device.class, "/api/device"));
-        gungnir.setSystem(bodyToMono(address, System.class, "/api/system"));
+        gungnir.setDetails(gungnirFacade.bodyToMono(address, Details.class, "/api/detail"));
+        gungnir.setDevice(gungnirFacade.bodyToMono(address, Device.class, "/api/device"));
+        gungnir.setSystem(gungnirFacade.bodyToMono(address, System.class, "/api/system"));
         return gungnir;
     }
 
     public void health(Address address) {
-        bodyToMono(address, Void.class, "/api/health");
-    }
-
-    private <K> K bodyToMono(Address address, Class<K> clazz, String contextPath) {
-        RestTemplate restTemplate = new RestTemplate();
-        restTemplate.getMessageConverters().add(httpMessageConverterToMono());
-        ResponseEntity<K> responseEntity = restTemplate.exchange(
-                address.getLocation().concat(contextPath),
-                HttpMethod.GET,
-                headersToMono(address.getUsername(), address.getPassword()),
-                clazz);
-        return responseEntity.getBody();
-    }
-
-    private HttpEntity<String> headersToMono(String username, String password) {
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setBasicAuth(username, password);
-        return new HttpEntity<>("body", httpHeaders);
-    }
-
-    private AbstractGenericHttpMessageConverter<Object> httpMessageConverterToMono() {
-        AbstractGenericHttpMessageConverter<Object> httpMessageConverter = new MappingJackson2HttpMessageConverter();
-        httpMessageConverter.setSupportedMediaTypes(Collections.singletonList(MediaType.TEXT_HTML));
-        return httpMessageConverter;
+        gungnirFacade.bodyToMono(address, Void.class, "/api/health");
     }
 }
