547d873c6fd18123e44346ec06a91674bdd60b62
hornokattila <hornokattila@outlook.com>
2022-02-15 15:07:52 +0100

    Crud repository improvement.

--- src/main/java/application/repository/AddressRepository.java
+++ src/main/java/application/repository/AddressRepository.java
@@ -1,12 +1,9 @@
 package application.repository;
 
 import application.entity.skofnung.database.Address;
-import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-import java.util.List;
-
 @RepositoryRestResource
-public interface AddressRepository extends Repository<Address, String> {
-    List<Address> findByLocation(String location);
+public interface AddressRepository extends CrudRepository<Address, String> {
 }
