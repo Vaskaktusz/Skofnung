3ae8ce9abfab91ad43fba62e5a986600a724264a
Attila Hornok <hornokattila@outlook.com>
2022-11-10 00:47:58 +0100

    Initial post test.

--- src/main/java/application/repository/DeployRepository.java
+++ src/main/java/application/repository/DeployRepository.java
@@ -3,7 +3,6 @@ package application.repository;
 import application.entity.skofnung.database.Source;
 import application.service.RestTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -12,7 +11,6 @@ public class DeployRepository {
     private RestTemplate restTemplate;
 
     public void save(Source source) {
-        // TODO: The actual file is not yet passed from the request to the call.
-        restTemplate.exchange(source, Void.class, HttpMethod.PUT, "/api/deploy");
+        restTemplate.httpPost(source, "/api/deploy");
     }
 }
--- src/main/java/application/service/RestTemplate.java
+++ src/main/java/application/service/RestTemplate.java
@@ -1,39 +1,43 @@
 package application.service;
 
 import application.entity.skofnung.database.Address;
-import org.springframework.http.HttpEntity;
+import application.entity.skofnung.database.Source;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClients;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.AbstractGenericHttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 @Service
 public final class RestTemplate {
+
+    public String httpPost(Source source, String contextPath) {
+        HttpPost request = new HttpPost(source.getLocation().concat(contextPath));
+        request.setHeader(HttpHeaders.AUTHORIZATION, getBasicAuthCode(source));
+        request.setHeader(HttpHeaders.CONTENT_TYPE, "text/html");
+        try {
+            HttpClient httpclient = HttpClients.createDefault();
+            HttpResponse response = httpclient.execute(request);
+            return new String(response.getEntity().getContent().readAllBytes());
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+        return null;
+    }
+
+    private String getBasicAuthCode(Address address) {
+        String auth = String.format("%s:%s", address.getUsername(), address.getPassword());
+        byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(StandardCharsets.ISO_8859_1));
+        return String.format("Basic %s", new String(encodedAuth));
+    }
+
     public <K> K exchange(Address address, Class<K> clazz, HttpMethod method, String contextPath) {
-        org.springframework.web.client.RestTemplate restTemplate = new org.springframework.web.client.RestTemplate();
-        restTemplate.getMessageConverters().add(setSupportedMediaTypes());
-        ResponseEntity<K> responseEntity = restTemplate.exchange(
-                address.getLocation().concat(contextPath),
-                method,
-                setBasicAuth(address.getUsername(), address.getPassword()),
-                clazz);
-        return responseEntity.getBody();
-    }
-
-    private HttpEntity<String> setBasicAuth(String username, String password) {
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setBasicAuth(username, password);
-        return new HttpEntity<>("body", httpHeaders);
-    }
-
-    private AbstractGenericHttpMessageConverter<Object> setSupportedMediaTypes() {
-        AbstractGenericHttpMessageConverter<Object> httpMessageConverter = new MappingJackson2HttpMessageConverter();
-        httpMessageConverter.setSupportedMediaTypes(Collections.singletonList(MediaType.TEXT_HTML));
-        return httpMessageConverter;
+        return null;
     }
 }
