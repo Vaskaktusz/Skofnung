3f688b97d49dc61e22c0df26fdc68ed9d9b9b69d
Attila Hornok <hornokattila@outlook.com>
2023-04-22 01:29:23 +0200

    Error handling for 403.

---
+++ src/main/java/application/service/ClientError.java
@@ -0,0 +1,15 @@
+package application.service;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.client.HttpClientErrorException;
+
+@ControllerAdvice
+public final class ClientError {
+
+    @ExceptionHandler(HttpClientErrorException.class)
+    public ResponseEntity<String> handle(HttpClientErrorException cee) {
+        return new ResponseEntity<>(cee.getStatusText(), cee.getStatusCode());
+    }
+}
