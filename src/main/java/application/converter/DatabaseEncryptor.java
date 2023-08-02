4950fb89da20ecb40aa2f49baba40ab45f4f2853
Attila Hornok <hornokattila@outlook.com>
2023-08-02 14:39:59 +0200

    Added comments.

--- src/main/java/application/converter/DatabaseEncryptor.java
+++ src/main/java/application/converter/DatabaseEncryptor.java
@@ -5,11 +5,13 @@ import jakarta.persistence.AttributeConverter;
 public final class DatabaseEncryptor implements AttributeConverter<String, String> {
     @Override
     public String convertToDatabaseColumn(String attribute) {
+        // TODO: Add encryption.
         return attribute;
     }
 
     @Override
     public String convertToEntityAttribute(String dbData) {
+        // TODO: Add decryption.
         return dbData;
     }
 }
