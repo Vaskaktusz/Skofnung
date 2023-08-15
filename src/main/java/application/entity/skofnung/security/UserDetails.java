fc7fb4907b7b62c27552142ee6b88b00e4efc0cf
Attila Hornok <hornokattila@outlook.com>
2023-08-15 10:37:57 +0200

    Final UserDetails using lombok for all fields.

--- src/main/java/application/entity/skofnung/security/UserDetails.java
+++ src/main/java/application/entity/skofnung/security/UserDetails.java
@@ -1,43 +1,28 @@
 package application.entity.skofnung.security;
 
-import application.converter.DatabaseEncryptor;
-import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 @Data
 @Entity
 public class UserDetails implements org.springframework.security.core.userdetails.UserDetails {
-    @Convert(converter = DatabaseEncryptor.class)
-    @Id
-    private String username;
-    @Convert(converter = DatabaseEncryptor.class)
+    private boolean accountNonExpired;
+    private boolean accountNonLocked;
+    private boolean credentialsNonExpired;
+    private boolean enabled;
     @NotBlank
     private String password;
-    private boolean enabled;
+    @Id
+    private String username;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
+        return new ArrayList<>();
     }
 }
