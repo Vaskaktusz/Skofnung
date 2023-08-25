de6ec388ed0bf0466d0865d9936c70df85214328
Attila Hornok <hornokattila@outlook.com>
2023-08-25 12:07:16 +0200

    Working security.

--- src/main/java/application/configuration/WebSecurity.java
+++ src/main/java/application/configuration/WebSecurity.java
@@ -3,14 +3,21 @@ package application.configuration;
 import application.repository.UserDetailsRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurity {
     @Bean
-    public UserDetailsService userDetailsService(UserDetailsRepository userDetailsRepository) {
-        return userDetailsRepository::findByUsername;
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity, UserDetailsRepository userDetailsRepository) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .userDetailsService(userDetailsRepository::findByUsername)
+                .httpBasic()
+                .and()
+                .build();
     }
 }
