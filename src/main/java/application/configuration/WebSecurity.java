80122540a64e590f2e726d6a10b6c0b9da33718c
Attila Hornok <hornokattila@outlook.com>
2023-08-30 16:56:56 +0200

    Final WebSecurity.java

--- src/main/java/application/configuration/WebSecurity.java
+++ src/main/java/application/configuration/WebSecurity.java
@@ -3,21 +3,26 @@ package application.configuration;
 import application.repository.UserDetailsRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurity {
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity, UserDetailsRepository userDetailsRepository) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
-                .userDetailsService(userDetailsRepository::findByUsername)
-                .httpBasic()
-                .and()
+                .httpBasic(SecurityConfigurerAdapter::and)
                 .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserDetailsRepository userDetailsRepository) {
+        return userDetailsRepository::findByUsername;
     }
 }
