9d2a18c80b15e60bcd32aaeb392c9c2168c40e8c
Attila Hornok <hornokattila@outlook.com>
2023-09-01 17:33:02 +0200

    Improved error handler with a generic INTERNAL_SERVER_ERROR.

--- src/main/java/application/service/ClientError.java
+++ src/main/java/application/service/ClientError.java
@@ -1,14 +1,21 @@
 package application.service;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestClientResponseException;
 
 @ControllerAdvice
 public final class ClientError {
-    @ExceptionHandler(HttpClientErrorException.class)
-    public ResponseEntity<String> handle(HttpClientErrorException cee) {
-        return new ResponseEntity<>(cee.getStatusText(), cee.getStatusCode());
+    @ExceptionHandler(RestClientResponseException.class)
+    public ResponseEntity<String> handle(RestClientResponseException rce) {
+        return new ResponseEntity<>(rce.getStatusText(), rce.getStatusCode());
+    }
+
+    @ExceptionHandler(RestClientException.class)
+    public ResponseEntity<String> handle(RestClientException rae) {
+        return new ResponseEntity<>(rae.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
