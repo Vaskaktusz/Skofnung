40b566c7af91ea63736f83baceab26d34af79fc3
Attila Hornok <hornokattila@outlook.com>
2024-06-04 13:11:15 +0200

    Initial logger impl.

--- src/main/java/application/controller/MemberController.java
+++ src/main/java/application/controller/MemberController.java
@@ -4,7 +4,6 @@ import application.entity.security.Member;
 import application.service.MemberService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
